0577048fe9be7006d3aa5bc292f1ca431d7ea284
Rinzlller <djsmaug2002@gmail.com>
2022-12-11 14:42:08 -0500

    Pwn_FlagBrute.py

--- desk.py
+++ desk.py
@@ -1,39 +1,59 @@
 #!/usr/bin/env python3
 
-# WORDLIST:	/usr/share/wordlists/rockyou.txt
+import requests
+import sys
+import re
+import random
 
-# import requests
-# from urllib.parse import quote, unquote			#unquote("%2F")		quote("\")
-# from html import unescape
-# from hashlib import md5
+flagRegEx = "[A-Z0-9]{31}="
+port = None # define port of service
+our_host = None # define it in attack sploit
+DEBUG = True # dont forgot change it in attack sploit
 
-# from pwn import *
-# from json import dumps							#dumps({"key":"value"})
-# from os import system
+alph = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789'
 
-# import string
+def idg( size = 8, chars=alph ):
+	return ''.join( random.choice( chars ) for _ in range( size ) )
 
-#===========================================================
-#                    EXPLOIT GOES HERE
-#===========================================================
+def login():
+	pass
 
-# <-->
-# <-<
-
-# -><
-# ->-
-
-def main():
-
-	a = 102078
-	b = 42019
-	p = 103837
-
-	for i in range(1000000):
-		if pow(a, i, p)==b:
-			print(i)
-			break
-
+def register():
+	pass
 
 if __name__ == "__main__":
-    main()
+
+	if len( sys.argv ) > 1:
+		host = sys.argv[ 1 ]
+	else:
+		print "Usage python " + sys.argv[ 0 ] + " <host>"
+		sys.exit( -1 )
+
+	if not our_host and not DEBUG:
+		print "Set our_host var!!!!"
+		sys.exit( -2 )
+	if not port:
+		print "Set port var!!!!"
+		sys.exit( -3 )
+
+	if host == our_host:
+		sys.exit( -4 )
+
+	host = 'http://' + host + ":" + str( port ) + "/"
+
+	print(idg())
+	
+	# sys.stdout.flush() # after print
+	# resp = requests.get( host )
+
+	# find flags and print on stdout
+	# flag_list = re.findall( flagRegEx, buf )
+	# for flag in flag_list:
+	#	print flag
+
+	# POST requests
+	# data = { 'key':'value', 'key1':'value1' }
+	# res = requests.post( host + "some-url", data=data )
+
+
+	# do not forgot about strip(), split(), replace() - functions to parsing
